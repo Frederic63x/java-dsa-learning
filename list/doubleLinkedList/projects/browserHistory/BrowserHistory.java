739f2e0190224c7126d9207e40202a376a1ac6a3
Seya <federicozuccara0@gmail.com>
2025-06-18 21:59:09 +0200

    fixed the askOperation logic, whit just a while that is always executed(true) and a return for case 0 to exit the function, also changed in this method the logic of the sc.nexInt() to parsing the sc.nextLine() as integer, cause sc.nextInt() doesn't take the new line but just the number, this causing problem in other functions (ex: skipping a line in the handleVisitPage method)

--- list/doubleLinkedList/projects/browserHistory/BrowserHistory.java
+++ list/doubleLinkedList/projects/browserHistory/BrowserHistory.java
@@ -9,12 +9,14 @@ public class BrowserHistory implements BrowserHistoryInterface {
         BrowserPage newPage = new BrowserPage(name, url);
         Node<BrowserPage> newNode = new Node<BrowserPage>(newPage);
         browserHistory.addInTail(newNode); // when i should not use addInTail? for the browserHistory is the only possible thing i think.
+        numberOfPageInHistory++;
     }
 
     // @params index: is the index choosen by the user via console of the Node<BrowserPage> inside the brwoserHistory DoubleLinkedList<BrowserPage>
     public void revisitPage(int index) {
         Node<BrowserPage> reVisitedPage = browserHistory.getAtIndex(index);
         browserHistory.addInTail(reVisitedPage);
+        numberOfPageInHistory++;
     }
 
     public Node<BrowserPage> getPageByIndex(int index) {
@@ -30,16 +32,19 @@ public class BrowserHistory implements BrowserHistoryInterface {
     }
 
     public BrowserPage removeAPage(int index) {
+        numberOfPageInHistory--;
         return new BrowserPage("", "");
     }
 
     public BrowserPage[] removePages(int[] indexes) {
         // just for test
         BrowserPage[] eliminatedPages = { new BrowserPage("", "") };
+        numberOfPageInHistory -= indexes.length; // or just = to browserHistory.size just know wich is better
         return eliminatedPages;
     }
 
     public int removeAllPages() {
+        numberOfPageInHistory = 0;
         return 0;
     }
 }
--- list/doubleLinkedList/projects/browserHistory/Main.java
+++ list/doubleLinkedList/projects/browserHistory/Main.java
@@ -8,67 +8,79 @@ public class Main {
 
     public static void main(String[] args) {
         BrowserHistory history1 = new BrowserHistory();
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("pippo");
+        askOperation(sc, history1);
+        // System.out.print("What's your name? ");
+        // String name = sc.nextLine();
 
-        System.out.println("pippo");
-        sc.nextLine();
-        sc.close();
+        // System.out.printf("So %s. How old are you? ", name);
+        // int age = sc.nextInt();
+
+        // System.out.printf(
+        //     "Cool! %d is a good age to start programming. \nWhat language would you prefer? ",
+        //     age
+        // );
+        // String language = sc.nextLine();
+
+        // System.out.printf("Ah! %s is a solid programming language.", language);
     }
 
     public static void askOperation(Scanner sc, BrowserHistory browserHistory) {
-        System.out.println(
-            "Possible Operation:\n" +
-            "0: exit the program" +
-            "1: visit a site with the url.\n" +
-            "2: revisit one of the last five visited page\n" +
-            "3: show the current Browser History\n" +
-            "4: delete a page from the history\n" +
-            "5: delete multiple page from the history\n" +
-            "6:  delete all the pages history"
-        );
         int choosenOperation;
-        do {
-            choosenOperation = sc.nextInt();
-            switch (choosenOperation) {
-                case 0:
-                    System.out.println("program terminated");
-                    break;
-                case 1:
-                    handleVisitPage(sc, browserHistory);
-                    break;
-                case 2:
-                    handleRevisitLastFive(sc, browserHistory);
-                    break;
-                case 3:
-                    browserHistory.printHistory();
-                    break;
-                default:
-                    System.out.println("function not yet implemented");
+        while (true) {
+            System.out.println(
+                "Possible Operation:\n" +
+                "0: exit the program\n" +
+                "1: visit a site with the url.\n" +
+                "2: revisit one of the last five visited page\n" +
+                "3: show the current Browser History\n" +
+                "4: delete a page from the history\n" +
+                "5: delete multiple page from the history\n" +
+                "6: delete all the pages history"
+            );
+            try {
+                choosenOperation = Integer.parseInt(sc.nextLine());
+                switch (choosenOperation) {
+                    case 0:
+                        System.out.println("program terminated");
+                        return;
+                    case 1:
+                        handleVisitPage(sc, browserHistory);
+                        // askOperatdion(sc, browserHistory);
+                        break;
+                    case 2:
+                        handleRevisitLastFive(sc, browserHistory);
+                        break;
+                    case 3:
+                        browserHistory.printHistory();
+                        break;
+                    default:
+                        System.out.println("function not yet implemented");
+                }
+            } catch (Exception e) {
+                System.out.println(
+                    "problem encountered in the execution of the program: " + e
+                );
             }
-        } while (
-            choosenOperation != 0 ||
-            choosenOperation != 1 ||
-            choosenOperation != 2 ||
-            choosenOperation != 3
-        );
-        sc.close();
+        }
     }
 
     public static void handleVisitPage(
         Scanner sc,
         BrowserHistory browserHistory
     ) {
-        System.out.println(browserHistory.getNumberOfPageInHistory());
+        // this is very bad cause it could be called not onlya sc.nextInt, so it's better the approach to do Integer.parseInt(sc.nextLine()) above
+        // String scannerNewLineCleaner = sc.nextLine();
 
-        System.out.println("insert the url of the page to visit");
+        System.out.print("insert it's name");
+        String name = sc.nextLine();
+
+        System.out.print("insert the url of the page to visit");
         String url = sc.nextLine();
 
-        System.out.println("insert it's name");
-        String name = sc.nextLine();
-        sc.close();
         browserHistory.visitNewPage(name, url);
+        System.out.println(browserHistory.getNumberOfPageInHistory());
+        browserHistory.printHistory();
     }
 
     public static void handleRevisitLastFive(
